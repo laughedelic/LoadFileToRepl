e77835002d656b7e959c07bbdddb15a127f9f499
Alexey Alekhin <alexey.alekhin@me.com>
2012-09-06 03:24:29 +0400

    init commit

---
+++ LoadFileToRepl.py
@@ -0,0 +1,58 @@
+import sublimerepl
+import sublime, sublime_plugin
+
+class LoadFileToReplCommand(sublime_plugin.TextCommand):
+	def run(self, edit, clear=False):
+		filename = self.view.file_name()
+		filetype = self.view.scope_name(0).split(" ")[0].split(".")[1]
+
+		source_view = self.view
+		source_group = source_view.window().active_group()
+
+		# if there is only one group change layout to 2 columns
+		if self.view.window().num_groups() == 1:
+			self.view.window().run_command("set_layout", {
+				"cols"  :  [0.0, 0.5, 1.0],            
+				"rows"  :  [0.0, 1.0],                 
+				"cells" :  [[0, 0, 1, 1], [1, 0, 2, 1]]
+				})
+		next_group = (source_group + 1) % self.view.window().num_groups()
+
+		if sublimerepl.find_repl(filetype) == None:
+			# focus on another group to open repl there
+			self.view.window().focus_group(next_group)
+			# open repl according to the type of source file
+			self.view.window().run_command("run_existing_window_command", {
+				"id"   :  "repl_" + filetype,
+				"file" :  "config/" + filetype.title() + "/Main.sublime-menu"
+			})
+
+		# reveal repl view, move and clear it if needed
+		repl_view = sublimerepl.find_repl(filetype)._view
+		self.view.window().focus_view(repl_view)
+		if source_group == repl_view.window().active_group():
+			repl_view.window().run_command("move_to_group", {"group": next_group})
+		if clear:
+			repl_view.run_command("repl_clear")
+
+		# focus back on source file
+		self.view.window().focus_view(source_view)
+
+		formats = {
+			  'haskell' :  ':load "%s"' 
+			, 'scala'   :  ':load %s'   
+			, 'clojure' :  '(load "%s")'
+			, 'ruby'    :  "load '%s'"  
+			, 'python'  :  'execfile("%s")'
+			}
+		text = formats[filetype] % filename
+
+		source_view.run_command("save")
+		self.view.window().run_command("repl_send", {
+			"external_id" :  filetype,
+			"text"        :  text,    
+			"file_name"   :  filename 
+			})
+
+		# just to show user that everything is ok
+		sublime.status_message(filetype.title() + " REPL > " + text)
---
+++ README.markdown
@@ -0,0 +1,33 @@
+# LoadFileToRepl Sublime Text 2 plugin
+
+## Description
+
+This is a companion plugin to [SublimeREPL](http://github.com/wuub/SublimeREPL) providing a command to load current source file into according REPL. It just uses a REPL command, like ':load "foo.hs"' for Haskell, or '(load-file "foo.clj")' for Clojure and etc.
+
+At the moment it supports Haskell, Scala, Clojure, Ruby and Python REPLs load commands, but it's easy to extend this list. Welcome to suggest yours!
+
+(Honestly, it was tested only with Haskell and Scala REPLs, so tell me please if it works fine with others).
+
+## Features (or just how it works)
+
+- Command 'load_file_to_repl'
+	* reveals REPL view or opens a new one according to the type of current file;
+	* always places REPL in another layout group (to see results simultaneously with code);
+	* optionally clears REPL before loading file;
+	* saves current file;
+	* and finally loads it into REPL, using according command.
+- Two hotkeys:
+	* 'super+r' — just runs 'load_file_to_repl' command;
+	* 'super+shift+r' — same, but clears REPL before loading file.
+
+## Installation
+
+I recommend you to use [Sublime Package Control](http://wbond.net/sublime_packages/package_control).
+* Install [SublimeREPL](http://github.com/wuub/SublimeREPL) plugin;
+* Add this repo as a new channel to Package Control and install this package.
+
+## TODO
+
+* More flexibility — through options (e.g layout) and smaller commands;
+* Add support for all SublimeREPL languages;
+* Submit this plugin to Community chanell.
